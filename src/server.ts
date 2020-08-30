d3334200addab2122be80cfb3560e08a95e36d72
Himujjal <upadhyayahimujjal@gmail.com>
2020-08-30 19:38:17 +0530

    first changes

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import * as sapper from "@sapper/server"; // eslint-disable-line import/no-unresolved
+import compression from "compression";
+import express, { Express } from "express";
+import sirv from "sirv";
+
+const PORT = process.env.PORT; // eslint-disable-line prefer-destructuring
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const createSapper = async (): Promise<Express> => {
+  const app = express();
+
+  app.use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  );
+
+  return app;
+};
+
+createSapper().then((app) => {
+  app.listen(PORT, (err?: any): void => {
+    // eslint-disable-line
+    if (err) console.log("error", err);
+  });
+});
